99d18fd8ca2a9e37f8e62d430bb843d101fb5cb0
RafaAudibert <rafaeelaudibert@gmail.com>
2020-06-05 01:20:00 -0300

    feat(points): Add endpoint with query for points

--- api/src/handlers/points/index.ts
+++ api/src/handlers/points/index.ts
@@ -4,11 +4,22 @@ import Point from '@models/point'
 import { Response } from '@handlers/types'
 
 // eslint-disable-next-line require-await
-export const handler = async ( _event: APIGatewayEvent ): Promise<Response> => {
-  try {
-    const points = await Point.all()
+export const handler = async ( event: APIGatewayEvent ): Promise<Response> => {
+  const { city, uf, items } = event.queryStringParameters
 
-    return success( { points } )
+  const parsedItems = items && items.split( ',' )
+
+  try {
+    const points = await Point.filter( JSON.parse( JSON.stringify( {
+      city,
+      uf
+    } ) ) )
+
+    const filteredPoints = parsedItems
+      ? points.filter( ( point ) => parsedItems.find( ( item ) => point.items.includes( item ) ) )
+      : points
+
+    return success( { points: filteredPoints } )
   } catch ( erro ) {
     return failure( erro )
   }
